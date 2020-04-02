a1e55f059ede82dfe3886e5dd893dacffd4f5201
Matt Vella <mattvella07@gmail.com>
2020-04-02 09:25:26 -0500

    Updated userdata

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -5,10 +5,11 @@ curl -sL https://rpm.nodesource.com/setup_13.x | sudo -E bash -
 sudo yum install nodejs -y
 sudo npm install -g pm2
 export NODE_ENV=qa
+echo "export NODE_ENV=qa" >> /etc/profile
 cd /home/ec2-user
 aws s3 cp s3://chore-divvy/chore-divvy-backend.tar .
 tar -xvf chore-divvy-backend.tar
 rm chore-divvy-backend.tar
 cd chore-divvy-backend
 npm install
-npm start
+pm2 start server.js --name chore-divvy-app
