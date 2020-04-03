6d7a78b310377f187944ad31e2501902d7b603fd
Matt Vella <mattvella07@gmail.com>
2020-04-02 20:54:47 -0500

    Added web module

---
+++ modules/web/outputs.tf
@@ -0,0 +1,3 @@
+output "public_ip" {
+    value = aws_instance.chore_divvy_web.public_ip
+}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "db_endpoint" {
 output "app_server_public_ip" {
     value = "App server running at ${module.app.public_ip}:8080"
 }
+
+output "web_server_public_ip" {
+    value = "Web server running at ${module.web.public_ip}:80"
+}
