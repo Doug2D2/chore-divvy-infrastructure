c737100bec8f3f5fea73a9240590048b68dc6577
Matt Vella <mattvella07@gmail.com>
2020-04-04 21:35:34 -0500

    Updated web userdata

--- modules/web/userdata.sh
+++ modules/web/userdata.sh
@@ -4,6 +4,8 @@ sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_13.x | sudo -E bash -
 sudo yum install nodejs -y
 sudo npm install -g serve
+export REACT_APP_BASE_URL=http://54.210.21.73:8080
+echo "export REACT_APP_BASE_URL=http://54.210.21.73:8080" >> /etc/environment
 cd /home/ec2-user
 aws s3 cp s3://chore-divvy/chore-divvy-frontend.tar .
 tar -xvf chore-divvy-frontend.tar
